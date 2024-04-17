fa899a25d464a24f15952d4b61c9f04f763c0e41
Marco Maroni <marcomaroni@gmail.com>
2024-04-17 10:20:19 +0200

    First commit

---
+++ README.md
@@ -0,0 +1,55 @@
+# Instagram To Bluesky
+
+Import all post exported from Instagram to a Bluesky account.
+
+⚠️ This project is a work-in-progress ⚠️
+
+They use the official archive export file format from Instagram (https://www.instagram.com/download/request), this utility reads the archive from the local disk and using the official Bluesky Typescript SDK imports the posts into the configured Bluesky account.
+
+⚠️ We recommend creating a specific account to test the import and not using your main Bluesky account ⚠️
+
+## Which posts are NOT imported
+
+- Stories and post with videos, because videos are not currently supported by Bluesky.
+
+## Prerequisite
+
+- Nodejs >= 20.12x
+- The archive of your post from the Instagram in your local disk.
+
+## Getting started
+
+1. Install Typescript: `npm i -g typescript`
+2. Install Node.js: `npm i -g ts-node`
+3. In the project folder run: `npm i`
+3. Create an .env file in the project folder by setting the following variables:
+- `BLUESKY_USERNAME` = username into which you want to import the tweets (e.g. "test.bsky.social")
+- `BLUESKY_PASSWORD` = account password created via App Password (eg. "pwd123")
+- `ARCHIVE_FOLDER` = full path to the folder containing the Instagram archive (e.g. "C:/Temp/instagram-archive")
+
+
+**I highly recommend trying to simulate the import first and import a small range of tweets, using the additional parameters documented below.**
+
+## Running the script 
+
+You can run the script locally: `npm start` or `npm run start_log` to write an import.log file.
+
+### Optional environment parameters
+
+Additionally you can set these environment variables to customize behavior:
+
+- `SIMULATE` = if set to "1" simulates the import by counting the tweets and indicating the estimated import time.
+- `MIN_DATE` = indicates the minimum date of tweets to import, ISO format (e.g. '2011-01-01' or '2011-02-09T10:30:49.000Z').
+- `MAX_DATE` = indicates the maximum date of tweets to import, ISO format (e.g. '2012-01-01' or '2014-04-09T12:36:49.328Z').
+
+## License
+
+"Instagram To Bluesky" is published under the MIT license.
+
+Copyright 2024 Marco Maroni
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the “Software”), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED “AS IS”, WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
---
+++ app.ts
@@ -0,0 +1,200 @@
+import * as dotenv from 'dotenv';
+import FS from 'fs';
+import * as process from 'process';
+
+import { BskyAgent, RichText } from '@atproto/api';
+
+dotenv.config();
+
+const agent = new BskyAgent({
+    service: 'https://bsky.social',
+})
+
+const SIMULATE = process.env.SIMULATE === "1";
+
+const API_DELAY = 2500; // https://docs.bsky.app/docs/advanced-guides/rate-limits
+
+const TWITTER_HANDLE = process.env.TWITTER_HANDLE;
+
+let MIN_DATE: Date | undefined = undefined;
+if (process.env.MIN_DATE != null && process.env.MIN_DATE.length > 0)
+    MIN_DATE = new Date(process.env.MIN_DATE as string);
+
+let MAX_DATE: Date | undefined = undefined;
+if (process.env.MAX_DATE != null && process.env.MAX_DATE.length > 0)
+    MAX_DATE = new Date(process.env.MAX_DATE as string);
+
+function decodeUTF8(data) {
+    if (typeof data === "string") {
+        const charCodes = Array.prototype.map.call(data, (c) => c.charCodeAt(0)) as number[];
+        const utf8 = new Uint8Array(charCodes);
+        return new TextDecoder("utf-8").decode(utf8);
+    }
+
+    if (Array.isArray(data)) {
+        return data.map(decodeUTF8);
+    }
+
+    if (typeof data === "object") {
+        const obj = {};
+        Object.entries(data).forEach(([key, value]) => {
+            obj[key] = decodeUTF8(value);
+        });
+        return obj;
+    }
+
+    return data;
+}
+
+
+async function main() {
+    console.log(`Import started at ${new Date().toISOString()}`)
+
+    const fInstaPosts = FS.readFileSync(process.env.ARCHIVE_FOLDER + "/your_instagram_activity/content/posts_1.json");
+    const instaPosts = decodeUTF8(JSON.parse(fInstaPosts.toString()));
+    let importedPosts = 0;
+    let importedMedia = 0;
+    if (instaPosts != null && instaPosts.length > 0) {
+
+        const sortedPost = instaPosts.sort((a, b) => {
+            let ad = new Date(a.media[0].creation_timestamp * 1000).getTime();
+            let bd = new Date(b.media[0].creation_timestamp * 1000).getTime();
+            return ad - bd;
+        });
+
+        await agent.login({ identifier: process.env.BLUESKY_USERNAME!, password: process.env.BLUESKY_PASSWORD! })
+
+        for (let index = 0; index < sortedPost.length; index++) {
+            const post = sortedPost[index];
+
+            let postDate: Date | undefined = undefined;
+            if (post.creation_timestamp != undefined)
+                postDate = new Date(post.creation_timestamp * 1000);
+
+            let postText = "";
+            if (post.title && post.title.length > 0)
+                postText = post.title;
+
+            // If the post is made up of a single image,
+            // the text of the post appears to be associated with the only image present
+            if ( post.media?.length == 1 ) {
+                if( postText.length == 0) {
+                    postText = post.media[0].title;
+                }
+                if( postDate == undefined ) {
+                    postDate = new Date(post.media[0].creation_timestamp * 1000);
+                }
+            }
+
+            //this cheks assume that the array is sorted by date (first the oldest)
+            if (MIN_DATE != undefined && postDate! < MIN_DATE)
+                continue;
+            if (MAX_DATE != undefined && postDate! > MAX_DATE)
+                break;
+
+            console.log(`Parse Instagram post'`);
+            console.log(` Created at ${postDate?.toISOString()}`);
+            console.log(` Text '${postText}'`);
+
+            let embeddedImage = [] as any;
+            for (let j = 0; j < post.media.length; j++) {
+                const postMedia = post.media[j];
+                const mediaDate = new Date(postMedia.creation_timestamp * 1000);
+                const mediaText = postMedia.title;
+
+                // if (postMedia.uri == "media/posts/202108/240742101_558570538822653_7921317535156034037_n_17968506598442521.jpg") {
+                //     console.log("debug");
+                // }
+
+                console.log(` Media ${j} - ${postMedia.uri}`);
+                console.log(`  Created at ${mediaDate.toISOString()}`);
+                console.log(`  Text '${mediaText}'`);
+
+                const fileType = postMedia.uri.substring(postMedia.uri.lastIndexOf(".") + 1)
+                let mimeType = "";
+                switch (fileType) {
+                    case "heic":
+                        mimeType = "image/heic"
+                        break;
+                    case "webp":
+                        mimeType = "image/webp"
+                        break;
+                    case "jpg":
+                        mimeType = "image/jpeg"
+                        break;
+                    default:
+                        console.error("Unsopported image file type" + fileType);
+                        break;
+                }
+                if (mimeType.length <= 0)
+                    continue;
+
+                const mediaFilename = `${process.env.ARCHIVE_FOLDER}/${postMedia.uri}`;
+                const imageBuffer = FS.readFileSync(mediaFilename);
+
+                if (!SIMULATE) {
+                    const blobRecord = await agent.uploadBlob(imageBuffer, {
+                        encoding: mimeType
+                    });
+
+                    embeddedImage.push({
+                        alt: mediaText,
+                        image: {
+                            $type: "blob",
+                            ref: blobRecord.data.blob.ref,
+                            mimeType: mimeType,
+                            size: blobRecord.data.blob.size
+                        }
+                    })
+                }
+
+                importedMedia++;
+            }
+
+            const rt = new RichText({
+                text: postText
+            });
+            await rt.detectFacets(agent);
+            const postRecord = {
+                $type: 'app.bsky.feed.post',
+                text: rt.text,
+                facets: rt.facets,
+                createdAt: postDate?.toISOString(),
+                embed: embeddedImage.length > 0 ? { $type: "app.bsky.embed.images", images: embeddedImage } : undefined,
+            }
+
+            if (!SIMULATE) {
+                //I wait 3 seconds so as not to exceed the api rate limits
+                await new Promise(resolve => setTimeout(resolve, API_DELAY));
+
+                const recordData = await agent.post(postRecord);
+                const i = recordData.uri.lastIndexOf("/");
+                if (i > 0) {
+                    const rkey = recordData.uri.substring(i + 1);
+                    const postUri = `https://bsky.app/profile/${process.env.BLUESKY_USERNAME!}/post/${rkey}`;
+                    console.log("Bluesky post create, URL: " + postUri);
+                    importedPosts++;
+                } else {
+                    console.warn(recordData);
+                }
+            } else {
+                importedPosts++;
+            }
+
+            // if( importedPosts > 2)
+            //     break;
+        }
+    }
+
+    if (SIMULATE) {
+        // In addition to the delay in AT Proto API calls, we will also consider a 10% delta for image upload
+        const minutes = Math.round((importedMedia * API_DELAY / 1000) / 60) * 1.10
+        const hours = Math.floor(minutes / 60);
+        const min = minutes % 60;
+        console.log(`Estimated time for real import: ${hours} hours and ${min} minutes`);
+    }
+
+    console.log(`Import finished at ${new Date().toISOString()}, imported ${importedPosts} posts with ${importedMedia} media`)
+}
+
+main();
