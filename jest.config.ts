c99670afdd97d445bf6f4c323057342949da397d
straiforos <stephen@traiforos.com>
2025-03-05 22:14:16 -0500

    Add codecov

--- jest.config.ts
+++ jest.config.ts
@@ -13,10 +13,23 @@ export default {
   },
   collectCoverage: true,
   coverageDirectory: 'coverage',
-  coverageReporters: ['text', 'lcov'],
+  coverageReporters: ['json', 'lcov', 'text', 'clover'],
   collectCoverageFrom: [
-    'src/**/*.{js,jsx,ts,tsx}',
-    '!src/**/*.d.ts',
-    '!src/**/*.test.{js,jsx,ts,tsx}'
-  ]
+    'src/**/*.ts',
+    '!src/**/*.test.ts',
+    '!src/**/*.spec.ts',
+    '!src/test/**',
+    '!src/**/index.ts',
+    '!src/main.ts'
+  ],
+  coverageThreshold: {
+    global: {
+      branches: 65,
+      functions: 80,
+      lines: 80,
+      statements: 80
+    }
+  },
+  forceExit: true,
+  detectOpenHandles: true
 }; 
