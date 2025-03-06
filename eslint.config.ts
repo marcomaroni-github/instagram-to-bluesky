c2a9571caacad7f4f7b3ee5bec942be0d2d75843
straiforos <stephen@traiforos.com>
2025-03-05 21:54:02 -0500

    Fix tsconfig error for including files not in the root.

--- eslint.config.ts
+++ eslint.config.ts
@@ -17,7 +17,7 @@ export default [
       parserOptions: {
         ecmaVersion: 'latest',
         sourceType: 'module',
-        project: './tsconfig.json'
+        project: ['./tsconfig.json', './tsconfig.root.json']
       },
       globals: {
         ...globals.node,
